9a82fe79aab8a4c9c100be4c1221c63f6e1270f3
ricardogk3 <rgk19@hotmail.com>
2021-09-08 14:31:46 -0300

    first commit

---
+++ web/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAOkt0R-30Q0n8nGlREH_IX8R2F9dGWPKg",
+    authDomain: "diariodeviagens-67479.firebaseapp.com",
+    projectId: "diariodeviagens-67479",
+    storageBucket: "diariodeviagens-67479.appspot.com",
+    messagingSenderId: "752960862549",
+    appId: "1:752960862549:web:e1eb2b270d327f156217dc",
+    measurementId: "G-XKLG1EVBEZ"
+  };
+
+    // Initialize Firebase
+    const app = firebase.initializeApp(firebaseConfig);
